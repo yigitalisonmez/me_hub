3ce502212fc57dffee5d005afb4b277795c11d8e
yigitalisonmez <y.sonmez0202@gmail.com>
2025-12-04 02:42:58 +0300

    refactored

--- lib/features/water/presentation/pages/water_page.dart
+++ lib/features/water/presentation/pages/water_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
